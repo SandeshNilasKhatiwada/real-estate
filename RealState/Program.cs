d9a46fb87d1b114ad9342ebe93258599660072a7
sandeshnilas <sandeshnilaskhatwada@gmail.com>
2025-01-24 18:17:26 +0545

    "user created and registered"

--- RealState/Program.cs
+++ RealState/Program.cs
@@ -1,21 +1,40 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RealState.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
-
-// Add DbContext with MySQL
 builder.Services.AddDbContext<RealStateContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
         new MySqlServerVersion(new Version(8, 0, 32))));
 
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<RealStateContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/Account/AccessDenied";
+});
+
 var app = builder.Build();
+
+// Seed roles
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    if (!await roleManager.RoleExistsAsync("Buyer"))
+        await roleManager.CreateAsync(new IdentityRole("Buyer"));
+    if (!await roleManager.RoleExistsAsync("Seller"))
+        await roleManager.CreateAsync(new IdentityRole("Seller"));
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
